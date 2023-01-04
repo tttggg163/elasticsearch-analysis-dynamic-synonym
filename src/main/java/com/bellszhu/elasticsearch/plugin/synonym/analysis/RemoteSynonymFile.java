3276eb406b405271287ac8bd88c7c66936cb644d
tttggg163 <tttggg163@163.com>
2023-01-04 11:34:39 +0800

    Revert "Explicitly close for ClosableHttpResponse method"

--- src/main/java/com/bellszhu/elasticsearch/plugin/synonym/analysis/RemoteSynonymFile.java
+++ src/main/java/com/bellszhu/elasticsearch/plugin/synonym/analysis/RemoteSynonymFile.java
@@ -90,7 +90,7 @@ public class RemoteSynonymFile implements SynonymFile {
     public SynonymMap reloadSynonymMap() {
         Reader rulesReader = null;
         try {
-            logger.debug("start reload remote synonym from {}.", location);
+            logger.info("start reload remote synonym from {}.", location);
             rulesReader = getReader();
             SynonymMap.Builder parser;
 
@@ -132,7 +132,7 @@ public class RemoteSynonymFile implements SynonymFile {
                 .setConnectionRequestTimeout(10 * 1000)
                 .setConnectTimeout(10 * 1000).setSocketTimeout(60 * 1000)
                 .build();
-        CloseableHttpResponse response = null;
+        CloseableHttpResponse response;
         BufferedReader br = null;
         HttpGet get = new HttpGet(location);
         get.setConfig(rc);
@@ -153,7 +153,7 @@ public class RemoteSynonymFile implements SynonymFile {
                 StringBuilder sb = new StringBuilder();
                 String line;
                 while ((line = br.readLine()) != null) {
-                    logger.debug("reload remote synonym: {}", line);
+                    logger.info("reload remote synonym: {}", line);
                     sb.append(line)
                             .append(System.getProperty("line.separator"));
                 }
@@ -172,13 +172,6 @@ public class RemoteSynonymFile implements SynonymFile {
                 }
             } catch (IOException e) {
                 logger.error("failed to close bufferedReader", e);
-            }
-            try {
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                logger.error("failed to close http response", e);
             }
         }
         return reader;
